29841327f4c3defc457ec6724c8b1c498f225c75
Cody Perry <codyperry65@yahoo.com>
2016-04-11 22:17:01 -0400

    Parse JSON data from API

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ var initialLocations = [
         description: "The Fitness Factory is an independent locally owned gym located in Pittsburgh’s ever expanding Shadyside neighborhood. We have been in business and serving the community for more than 20 years. Our fitness philosophy focuses on a combination of strength, cardio and wellness.",
         latlng: { lat: 40.458458, lng: -79.925130 },
         website: "http://fitnessfactorypgh.com/",
+        phone: "(412) 362-6303",
         twitter: "FitFactoryPGH",
+        twitterLink: "https://twitter.com/FitFactoryPGH?lang=en",
         query: "FitnessFactory"
     },
     {
@@ -15,7 +17,9 @@ var initialLocations = [
         description: "At MÜV Integrated, we believe that movement and fitness should only be based on the science of the human body. We are confident in our ability to take anyone from a baseline and utilize Strategic Resistance™ manipulation to build postural endurance, strength, conditioning, mobility, speed, and power.",
         latlng: { lat: 40.464666, lng: -79.933382 },
         website: "http://www.muvintegrated.com/",
+        phone: "(412) 477-5069",
         twitter: "muvintegrated",
+        twitterLink: "https://twitter.com/muvintegrated?lang=en",
         query: "MUV"
     },
     {
@@ -24,7 +28,9 @@ var initialLocations = [
         description: "Today, the Y engages more than 10,000 neighborhoods across the U.S. As the nation’s leading nonprofit committed tohelping people and communities to learn, grow and thrive, our contributions are both far-reaching and intimate—from influencing our nation’s culture during times of profound social change to the individual support we provide an adult learning to read.",
         latlng: { lat: 40.440763, lng: -80.001168 },
         website: "http://www.ymcaofpittsburgh.org/locations/pnc-ymca/",
+        phone: "(412) 471-9622",
         twitter: "PNCYMCA",
+        twitterLink: "https://twitter.com/PNCYMCA?lang=en",
         query: "PNCYMCA"
     },
     {
@@ -33,7 +39,9 @@ var initialLocations = [
         description: "Since 1994, X Shadyside 24 Hour Health and Fitness has been a leader in providing the most modern fitness solutions in the Shadyside, Squirrel Hill, Oakland, and other East End communities of Pittsburgh. Recognized for our convenient location, contemporary equipment, and strong group exercise program; we are absolutely dedicated to providing our members with the best in fitness!",
         latlng: { lat: 40.451574, lng: -79.931729 },
         website: "http://www.xshadyside.com/",
+        phone: "(412) 363-9999",
         twitter: "XShadyside",
+        twitterLink: "https://twitter.com/XShadyside?lang=en",
         query: "XShadyside"
     },
     {
@@ -42,7 +50,9 @@ var initialLocations = [
         description: "Orangetheory Fitness offers 60-minute workout sessions split into intervals of cardiovascular and strength training with heart rate monitors to track intensity and maximize metabolic burn. Increase energy, get visible results and burn more calories, even after leaving the studio. That's the Orange Effect!",
         latlng: { lat: 40.462251, lng: -79.927118 },
         website: "http://www.orangetheoryfitness.com/",
+        phone: "(412) 362-3616",
         twitter: "OrangeTheoryGVR",
+        twitterLink: "https://twitter.com/OrangeTheoryGVR?lang=en",
         query: "OrangetheoryFitness"
     },
     {
@@ -51,7 +61,9 @@ var initialLocations = [
         description: "Our aim is to change the climate of the fitness industry by providing a dynamic, energetic, and comprehensive platform for our clients.",
         latlng: { lat: 40.459523, lng: -79.928313 },
         website: "http://www.shapetrain.com/",
+        phone: "(412) 996-5016",
         twitter: "Shapetrain412",
+        twitterLink: "https://twitter.com/Shapetrain412?lang=en",
         query: "ShapeTraining"
     },
     {
@@ -60,7 +72,9 @@ var initialLocations = [
         description: "Get fit with a membership to Gold’s Gym Pittsburgh (Market Square), located in Pittsburgh, PA. With our exceptional health club, you’ll reach your fitness goals in no time, whether you’re aiming to lose weight, flatten your abs or shred upper body muscles. Our world-class fitness experts are always ready to provide workout tips and sound fitness advice. Members at Gold’s Gym Pittsburgh (Market Square) enjoy a state-of-the-art workout facility, top-tier amenities and the best fitness options in the industry.",
         latlng: { lat: 40.440900, lng: -80.003742 },
         website: "http://www.goldsgym.com/",
+        phone: "(412) 201-4653",
         twitter: "GoldsGym",
+        twitterLink: "https://twitter.com/GoldsGym?lang=en",
         query: "GoldsGym"
     },
     {
@@ -69,7 +83,9 @@ var initialLocations = [
         description: "Consistent support to answer any question or problem you may have. Lose weight, increase muscle tone and feel more confident than ever! A greater range of motion and joint movement to keep you healthy. Learn about and implement a diet that will transform your physique.",
         latlng: { lat: 40.460252, lng: -79.971044 },
         website: "http://panthrofitness.com/",
+        phone: "(412) 391-0100",
         twitter: "PanthroFitness",
+        twitterLink: "https://twitter.com/PanthroFitness?lang=en",
         query: "PanthroFitness"
         
     },
@@ -79,7 +95,9 @@ var initialLocations = [
        description: "Everyone has individual goals, and we will work together to meet them. I talk to each client to understand their strengths, weaknesses and limitations. I then design a personal training program based on this information. I help my clients achieve, maintain, and even exceed what they once thought was impossible.",
        latlng: { lat: 40.458625, lng: -79.931998 },
        website: "http://www.terra-fitness.com/",
+       phone: "(412) 901-3645",
        twitter: "terra_fitness",
+       twitterLink: "https://twitter.com/terra_fitness/with_replies?lang=en",
        query: "terraFITNESS"
     },
     {
@@ -88,7 +106,9 @@ var initialLocations = [
         description: "Think about this question for a second… Where can you find a Fitness Center, Basketball/Volleyball Courts, Badminton, 3 giant indoor turf fields that can be used for: (Soccer, Baseball, Softball, Football, Kickball, Lacrosse, Frisbee, etc…), and Indoor Batting Cages and Live Pitching Tunnels. Oh, by the way we also have our own CrossFit Gym section, D1 Sports Performance Training Gym section, Warriors Fitness Kickboxing Gym section, plus a Salsa Dance Studio section. So instead of the question of why should someone want to join our gym, lets rephrase that to “Why shouldn’t someone want to join our gym?”",
         latlng: { lat: 40.420392, lng: -80.058305 },
         website: "https://greentreesportsplex.com/",
+        phone: "(412) 922-1818",
         twitter: "TheSportsPlex",
+        twitterLink: "https://twitter.com/TheSportsPlex?lang=en",
         query: "Sportsplex"
     }
     
@@ -216,7 +236,7 @@ function ViewModel() {
         self.query('');
     });
     
-    //FourSquare API Request 
+    //FourSquare API Request | declare reference properties
         var clientID = '54I1UHM3AGCPB45VNROLAC23DEARBPZ3R4C0Y5QX3BDL30SV';
         var clientSecret = 'SCQYO3CFBWVOHBYK4HUMBBXF3VUIF5ETZPD01BXTGBO2YW00';
         var fqVersion = '20130815';
@@ -230,17 +250,23 @@ function ViewModel() {
             llLng = req.latlng.lng;
             fourSqUrl = 'https://api.foursquare.com/v2/venues/explore' + '?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + fqVersion + '&ll=' + llLat + ',' + llLng + '&query=' + req.query + '&limit=50';
             
-            console.log(fourSqUrl);
+            //Make ajax call to Foursquare API to get requested Data
             $.ajax({
             url: fourSqUrl,
             datatype: "jsonp",
             success: function(response) {
-                console.log(response.response.totalResults);
+                var resp = response.response.groups[0].items[0].venue;
+                console.log(req.name);
+                
+                //Build infoWidow content string with data from API Request
+                infoWindow.setContent(resp.name + '<br>' + req.phone + '<br>' + resp.location.address + '<br>' + resp.location.city + ', ' + resp.location.state + ' ' + resp.location.postalCode + '<br>'  + '<a href="' + req.website + '">' + req.website + '</a>' + '<br>' + '<a href="' + req.twitterLink + '">' + '@' + req.twitter + '</a>');
+
+
             }
             
         });
             
-        });
+    });
     
      
 };
